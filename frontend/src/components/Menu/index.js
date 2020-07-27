08cd5fba695e4740974241e4c0fbb6bfcbbd8ae4
Stalk3 <lucasefftingn@hotmail.com>
2020-07-27 12:17:28 -0300

    KovaLogo

--- frontend/src/components/Menu/index.js
+++ frontend/src/components/Menu/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../../assets/img/Logo2.png';
+import Logo from '../../assets/img/KovaLogo.png';
 import Button from '../Button'
 // import ButtonLink from './components/ButtonLink';
 import './Menu.css'
